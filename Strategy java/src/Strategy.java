7aa9d953b3f47d98bd46426e8f7489c76a8dbcbb
SaintsDump <148344865+SaintsDump@users.noreply.github.com>
2023-10-20 14:52:08 +0800

    Add files via upload

---
+++ Strategy java/src/Strategy.java
@@ -0,0 +1,55 @@
+
+interface Payment{
+    public void pay(double amount)  ;
+}
+
+class Online implements Payment{
+    private String email;
+    private String password;
+
+    public Online(String email, String password){
+        this.email = email;
+        this.password = password;
+
+    }
+    public void pay(double amount){
+
+        System.out.println("Paid using online account "+ amount);
+    }
+}
+
+class Mobile implements Payment{
+
+    private String number;
+    private int pin;
+
+    public Mobile(String number, int pin){
+        this.number= number;
+        this.pin=pin;
+    }
+    public void pay(double amount){
+        System.out.println("Paid using mobile wallet " + amount);
+    }
+}
+
+class Cart{
+
+    private double amount;
+
+    public Cart(double amount){
+        this.amount = amount;
+    }
+    public void pay(Payment mode){
+        mode.pay(amount);
+    }
+}
+public class Strategy {
+
+    public static void main(String[] args) {
+        Cart cart = new Cart(1512.75);
+        cart.pay(new Online("ReymondBungo@gmail.com", "Wasd8456!"));
+        cart = new Cart(375.255);
+        cart.pay(new Mobile("09168785199", 1234));
+    }
+
+}
